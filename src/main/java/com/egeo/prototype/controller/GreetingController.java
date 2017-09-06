0771899397d89e886402570bbdc20060b8c2cc8e
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-06 19:12:45 +0300

    prototype is ready

--- src/main/java/com/egeo/prototype/controller/GreetingController.java
+++ src/main/java/com/egeo/prototype/controller/GreetingController.java
@@ -5,8 +5,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 @Controller
 public class GreetingController {
+
+    private static final String YANDEX_API = "https://api-maps.yandex.ru/2.1/?lang=en_RU";
+
+    private static final String DEFAULT_SCRIPT = "js/map-api-clean-map.js";
+
+    private static final Map<String, String> SCRIPTS = new HashMap<>();
+    static {
+        SCRIPTS.put("polygon", "js/map-api-polygon.js");
+        SCRIPTS.put("polygon-with-image", "js/map-api-polygon-with-image.js");
+        SCRIPTS.put("custom-icons", "js/icon_customImage.js");
+        SCRIPTS.put("custom-lines", "js/map-api-polyline.js");
+        SCRIPTS.put("map-editor", "js/map-api-editor.js");
+    }
+
+    /*
+
+    <li><a href="index.html?script=polygon">Polygon</a></li>
+                            <li><a href="index.html?script=polygon-with-image">Polygon with image</a></li>
+                            <li><a href="index.html?script=custom-icons">Custom icons</a></li>
+                            <li><a href="index.html?script=custom-lines">Custom lines</a></li>
+                            <li><a href="index.html?script=map-editor">Map editor</a></li>
+
+     */
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,11 +41,15 @@ public class GreetingController {
         return "greeting";
     }
 
-    @RequestMapping("/map")
-    public String map(@RequestParam(value="script", required=false, defaultValue="js/map-api-clean-map.js") String script,
+    @RequestMapping("/")
+    public String map(@RequestParam(value="script", required=false, defaultValue="default") String script,
                            Model model) {
-        model.addAttribute("script", script);
-        return "map";
+        String js = Optional.ofNullable(SCRIPTS.get(script)).orElse(DEFAULT_SCRIPT);
+        model.addAttribute("script", js);
+        String api = "map-editor".equals(script) ? YANDEX_API + "&coordorder=longlat" : YANDEX_API;
+        model.addAttribute("api", api);
+
+        return "index";
     }
 
 }
--- src/main/resources/static/js/map-api-editor.js
+++ src/main/resources/static/js/map-api-editor.js
@@ -1,7 +1,7 @@
 ymaps.ready(init);
 
 function init() {
-    // Создаем карту.
+    // Creating the map.
     var myMap = new ymaps.Map("map", {
         center: [90, 15],
         zoom: 3,
@@ -10,68 +10,72 @@ function init() {
         searchControlProvider: 'yandex#search'
     });
 
-    // Создаем ломаную.
+    // Creating a polyline.
     var myPolyline = new ymaps.Polyline([
-        // Указываем координаты вершин.
+        // Specifying the coordinates of the vertices.
         [70, 20],
         [70, 40],
         [90, 15],
         [70, -10]
     ], {}, {
-        // Задаем опции геообъекта.
-        // Цвет с прозрачностью.
+        /**
+         * Setting geo object options.
+         * Color with transparency.
+         */
         strokeColor: '#FF008888'
     });
 
     myPolyline.editor.options.set({
-        // Задаём для меток вершин опции с постфиксами, привязанными к текущему состоянию вершины.
-        // Класс макета для меток на вершинах ломаной линии.
+        /**
+         * Specifying options with postfixes linked to the current state of the vertex.
+         * The layout class for placemarks on the vertices of a polyline.
+         */
         vertexLayout: 'default#image',
-        // URL графического файла.
+        // URL of the image file.
         vertexIconImageHref: 'images/button3.png',
-        // Размеры слоя с картинкой.
+        // Dimensions of the image layer.
         vertexIconImageSize: [16, 16],
-        // Смещение картинки относительно точки привязки.
+        // Offset of the image relative to the anchor point.
         vertexIconImageOffset: [-8, -8],
 
-        // Опции с данным постфиксом применяются при наведении на вершину указателя мыши.
+        // Options with this given postfix are used when the user hovers over a vertex with the mouse pointer.
         vertexLayoutHover: 'default#image',
         vertexIconImageSizeHover: [28, 28],
         vertexIconImageOffsetHover: [-14, -14],
 
-        // Опции с данным постфиксом применяются, когда для вершины открыто контекстное меню.
+        // Options with this given postfix are used when the context menu is open on a vertex.
         vertexLayoutActive: 'default#image',
         vertexIconImageHrefActive: 'images/button4.png',
         vertexIconImageSizeActive: [16, 16],
         vertexIconImageOffsetActive: [-8, -8],
 
-        // Опции с данным постфиксом применяются при перетаскивании вершины.
+        // Options with this given postfix are used when the user drags a vertex.
         vertexLayoutDrag: 'default#image',
         vertexIconImageHrefDrag: 'images/button4.png',
         vertexIconImageSizeDrag: [16, 16],
         vertexIconImageOffsetDrag: [-8, -8],
 
-        // Задаём для промежуточных меток опции с постфиксами, привязанными к текущему состоянию промежуточных меток.
+        // Specifying options with postfixes for interim placemarks which are linked to the current state of the interim placemarks.
         edgeLayout: 'default#image',
         edgeIconImageHref: 'images/button1.png',
         edgeIconImageSize: [16, 16],
         edgeIconImageOffset: [-8, -8],
 
-        // Опции с данным постфиксом применяются при наведении на промежуточную метку указателя мыши.
+        // Options with this given postfix are used when the user hovers over an interim placemark with the mouse pointer.
         edgeLayoutHover: 'default#image',
         edgeIconImageSizeHover: [28, 28],
         edgeIconImageOffsetHover: [-14, -14],
 
-        // Опции с данным постфиксом применяются при перетаскивании промежуточной метки.
+        // Options with this given postfix are used when the user drags an interim placemark.
         edgeLayoutDrag: 'default#image',
         edgeIconImageHrefDrag: 'images/button2.png',
         edgeIconImageSizeDrag: [16, 16],
         edgeIconImageOffsetDrag: [-8, -8]
     });
 
-    // Добавляем линию на карту.
+    // Adding a line to the map.
     myMap.geoObjects.add(myPolyline);
 
-    // Включаем режим редактирования.
+    // Turning on the edit mode.
     myPolyline.editor.startEditing();
 }
